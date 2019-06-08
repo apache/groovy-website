af4cec43d3e18434f68c3868429c646cdeff4db0
Szymon Stepniak <szymon.stepniak@gmail.com>
2019-06-08 21:14:57 +0200

    fix contribute button branch

--- site/src/site/includes/contribute-button.groovy
+++ site/src/site/includes/contribute-button.groovy
@@ -1,6 +1,6 @@
 div(id: 'contribute-btn') {
     button(type: 'button', class: 'btn btn-default',
-            onclick: "window.location.href=\"https://github.com/apache/groovy-website/tree/master/site/src/site/pages/${currentPage}.groovy\"") {
+            onclick: "window.location.href=\"https://github.com/apache/groovy-website/tree/asf-site/site/src/site/pages/${currentPage}.groovy\"") {
         i(class: 'fa fa-pencil-square-o') {}
         yield ' Improve this doc'
     }
