7eae96241d38e926df41c15a40d4958b8578a954
Paul King <paulk@asert.com.au>
2020-02-13 15:06:46 +1000

    CI changes

--- site/src/site/pages/buildstatus.groovy
+++ site/src/site/pages/buildstatus.groovy
@@ -47,10 +47,10 @@ layout 'layouts/main.groovy', true,
                                     }
                                 }
                                 renderBuilds([
-                                        'Groovy master, JDK 11': ['GroovyMasterTestAllJdk11', ''],
-                                        'Groovy master, JDK 8': ['GroovyMasterTestAllJdk8', ''],
+                                        'Groovy master, JDK 11': ['GroovyTestAllJdk11', ''],
+                                        'Groovy master, JDK 8': ['GroovyTestAllJdk8', ''],
                                         'Groovy 3.0.X, JDK 8': ['Groovy30xTestAllJdk8', ''],
-                                        'Groovy 2.5.X, JDK 11': ['GroovyGroovy25xCheckJdk11', ''],
+                                        'Groovy 2.5.X, JDK 11': ['Groovy25xCheckJdk11', ''],
                                         'Groovy 2.4.X, JDK 8': ['Groovy24xCheckJdk8', '']
                                 ])
 
