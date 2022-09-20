4d4fe2b2833f88c9be82cbba4ada027f07559023
Paul King <paulk@asert.com.au>
2022-09-20 18:26:40 +1000

    add windows installer

--- site/src/site/sitemap-dev.groovy
+++ site/src/site/sitemap-dev.groovy
@@ -103,12 +103,7 @@ downloads {
         }
         version('3.0.13') {
             stable true
-//            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-3.0.13/'
-        }
-        version('3.0.12') {
-            stable true
-            archive true
-            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-3.0.12/'
+            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-3.0.13/'
         }
     }
     distribution('Groovy 2.5') {
