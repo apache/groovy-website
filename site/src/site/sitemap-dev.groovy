993cf4c13d58e3c76c26260c914d61609299b3af
Paul King <paulk@asert.com.au>
2023-01-22 15:11:17 +1000

    Release 2.5.21: update sitemap

--- site/src/site/sitemap-dev.groovy
+++ site/src/site/sitemap-dev.groovy
@@ -115,8 +115,13 @@ downloads {
             a(href: 'versioning.html', 'version')
             yield ' of Groovy still in widespread use.'
         }
+        version('2.5.21') {
+            stable true
+//            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-2.5.21/'
+        }
         version('2.5.20') {
             stable true
+            archive true
             windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-2.5.20/'
         }
     }
