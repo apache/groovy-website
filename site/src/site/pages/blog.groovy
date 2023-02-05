bda48f3e494c8656104bb8960c21b1756af99dad
Paul King <paulk@asert.com.au>
2023-02-05 14:01:50 +1000

    migrate some blog posts

--- site/src/site/pages/blog.groovy
+++ site/src/site/pages/blog.groovy
@@ -39,7 +39,7 @@ layout 'layouts/main.groovy', true,
                         div(class: 'col-lg-3') {
                             ul(class: 'nav-sidebar') {
                                 li {
-                                    a(href: '..', 'Blog index')
+                                    a(href: '/blog/', 'Blog index')
                                 }
                                 li(class:'active') {
                                     a(href: '#doc', title)
