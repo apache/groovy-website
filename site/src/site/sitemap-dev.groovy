7f14339563517fd1b46fc0afc77610fb613a4c9a
Paul King <paulk@asert.com.au>
2023-06-29 17:09:40 +1000

    Release 3.0.18: update sitemap

--- site/src/site/sitemap-dev.groovy
+++ site/src/site/sitemap-dev.groovy
@@ -109,8 +109,13 @@ downloads {
             a(href: 'versioning.html', 'version')
             yield ' of Groovy designed for JDK8+ with a new more flexible parser (aka Parrot parser).'
         }
+        version('3.0.18') {
+            stable true
+//            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-3.0.18/'
+        }
         version('3.0.17') {
             stable true
+            archive true
             windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-3.0.17/'
         }
     }
