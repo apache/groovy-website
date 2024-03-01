d4019b71d57c49aa65ab9a59b5d2dd43d2428d7c
Paul King <paulk@asert.com.au>
2024-03-01 21:37:42 +1000

    Release 4.0.19: update sitemap

--- site/src/site/sitemap-dev.groovy
+++ site/src/site/sitemap-dev.groovy
@@ -101,8 +101,13 @@ downloads {
             a(href: 'versioning.html', 'version')
             yield ' of Groovy designed for JDK8+ with much improved JPMS support.'
         }
+        version('4.0.19') {
+            stable true
+//            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-4.0.19/'
+        }
         version('4.0.18') {
             stable true
+            archive true
             windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-4.0.18/'
         }
     }
