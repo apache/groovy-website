8d7d6c2f215f7502e846c6c143893391a4579ce5
Paul King <paulk@asert.com.au>
2026-09-07 21:42:39 +1000

    Release 6.0.0-RC-1: update sitemap

--- site/src/site/sitemap-dev.groovy
+++ site/src/site/sitemap-dev.groovy
@@ -87,13 +87,14 @@ downloads {
             a(href: 'versioning.html', 'version')
             yield ' of Groovy designed for JDK17+. Alpha releases are not recommended for production use.'
         }
+        version('6.0.0-RC-1') {
+            stable false
+//            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-6.0.0-RC-1/'
+        }
         version('6.0.0-beta-3') {
             stable false
-//            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-6.0.0-beta-3/'
-        }
-        version('6.0.0-beta-2') {
-            stable false
-            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-6.0.0-beta-2/'
+            archive true
+            windowsInstaller 'https://groovy.jfrog.io/artifactory/dist-release-local/groovy-windows-installer/groovy-6.0.0-beta-3/'
         }
     }
     distribution('Groovy 5.1') {
